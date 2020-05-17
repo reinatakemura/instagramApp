9d9694475b29e2ff72b76d01b482879834ff0497
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-17 16:06:10 +0900

    Edit your profile without password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true, length: { maximum: 50 }
+
+  def update_without_current_password(params, *options)  #<% passwordなしで編集を可能にする %>
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)  #<% key に対応する要素を取り除く %>
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)  #<% 引数で指定したHashオブジェクトで更新 %>
+    clean_up_passwords  #<% Object.passwordとpassword_confirmationの値をnilにする %>
+    result
+  end
 end
