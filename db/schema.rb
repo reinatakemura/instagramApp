044fbe5c8b963f58f4615ad74cc6c1262986c5be
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-19 10:17:31 +0900

    create photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_011103) do
+ActiveRecord::Schema.define(version: 2020_05_19_011638) do
+
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_photos_on_post_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "caption"
@@ -34,5 +42,6 @@ ActiveRecord::Schema.define(version: 2020_05_19_011103) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end
