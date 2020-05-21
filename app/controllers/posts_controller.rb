cd9eaa90afba7fd56e36ee9893b19b479e6569d7
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-22 00:38:26 +0900

    creating posts_controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+    @post.photos.build  # モデルを関連付けして、newインスタンスを作成
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.photos.present?
+      @post.save
+      redirect_to root_path
+      flash[:notice] = "投稿が保存されました"
+    else
+      redirect_to root_path
+      flash[:notice] = "投稿に失敗しました"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
+  end
+end
