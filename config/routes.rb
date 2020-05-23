bb8a7bb752fb03223e8b8e8c7a717414fa694c0f
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-24 00:09:44 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'user' # 後でresourcesに変更する
 
-  resources :posts, only: [:new, :create] do
+  resources :posts, only: [:index, :new, :create] do
     resources :photos, only: [:create]
   end
 end
