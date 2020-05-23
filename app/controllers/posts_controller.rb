4c54f55130bb7d7e2223c0d8719eb530407afb4f
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-24 00:15:24 +0900

    Editing posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!  # 投稿された画像はサインインしたユーザーのみに見える
 
+  def index
+    # gem kaminariにて、ページごとに10投稿ずつ表示するようにする
+    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC') # 関連するテーブルをまとめて取得するincludesメソッド
+  end
+
   def new
     @post = Post.new
     @post.photos.build  # モデルを関連付けして、newインスタンスを作成
