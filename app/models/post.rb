f54b4ef7603f5cec2211434b4f5589177a9a4068
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-06-01 21:35:23 +0900

    Create likes views

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   has_many :likes, -> {order(created_at: :desc)}, dependent: :destroy # orderメソッドは、関連付けられたオブジェクトに与えられる順序を指定します。内部的にはSQLのORDER BY句が使われる
 
   accepts_nested_attributes_for :photos
+
+  def liked_by(user)
+    Like.find_by(user_id: user.id, post_id: id) # user_idとpost_idが一致するlikeを探し、なければnilを返す
+  end
 end
